af20e83dc8e79075bf19d2fbde391deba98ffd27
Diego Costa <diego@diegocosta.com.br>
2018-01-19 21:12:28 -0300

    Alterado as conexões do DB para acessar o MySQL rodando no container do Docker

--- api/config.php
+++ api/config.php
@@ -1,6 +1,6 @@
 <?php
 define('SERVER', 'D');
-define('MYSQL_HOST', 'localhost');
+define('MYSQL_HOST', 'mysql');
 define('MYSQL_PORT', '3306');
 define('MYSQL_USER', 'selecao_fullstack');
 define('MYSQL_PASS', 'barcelona');
