94b8e95d9a313dd1c03435d1661213b31c3fbfa5
Diego Costa <diego@diegocosta.com.br>
2018-01-20 08:41:27 -0300

    Alterado a forma que as configurações são carregadas pelo .env

--- api/index.php
+++ api/index.php
@@ -1,13 +1,38 @@
 <?php
+require_once 'vendor/autoload.php';
 
-require 'vendor/autoload.php';
+// Required environment variables
+$envConfig = [
+    'SERVER', 
+    'MYSQL_HOST', 
+    'MYSQL_PORT', 
+    'MYSQL_USER', 
+    'MYSQL_PASS',
+    'MYSQL_BASE',
+    'MYSQL_CHARSET',
+    'MYSQL_TIMEZONE',
+    'PHP_TIMEZONE'
+];
+
+// Load variables
+try {
+    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv->load();
+    $dotenv->required($envConfig);
+
+    foreach($envConfig as $env) {
+        define($env, getenv($env));
+    }
+} catch(Exception $e) {
+    echo $e->getMessage();
+    die;
+}
 
 $settings['displayErrorDetails'] = true;
 $settings['determineRouteBeforeAppMiddleware'] = true;
 
 $app = new \Slim\App(["settings" => $settings]);
 
-require 'config.php';
 require '_genesis/genesis.php';
 require 'endpoints/usuarios.php';
 require 'endpoints/animais.php';
