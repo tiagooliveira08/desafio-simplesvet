368c5bc23e24cd6ed374b59b6d69b37bed46877f
Diego Costa <diego@diegocosta.com.br>
2018-01-21 15:31:37 -0300

    Corrigido problemas apontados pelo CodeSniffer

--- api/src/Lite/middlewares.php
+++ api/src/Lite/middlewares.php
@@ -19,7 +19,7 @@ $app->add(new \Slim\Middleware\JwtAuthentication([
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
