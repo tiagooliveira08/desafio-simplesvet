194e3daf2e15bfe6e5c6229d61ddc2be1c3283ea
Diego Costa <diego@diegocosta.com.br>
2018-01-21 15:29:22 -0300

    Adicionado middleware para desabilitar o CORS

--- api/src/Lite/middlewares.php
+++ api/src/Lite/middlewares.php
@@ -15,3 +15,11 @@ $app->add(new \Slim\Middleware\JwtAuthentication([
         return $response->withJson($data, 500);
     }
 ]));
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
