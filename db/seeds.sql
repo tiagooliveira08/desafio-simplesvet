50afdc5e5bbd024b8dbb30b832f9d8d63cf4544a
Diego Costa <diego@diegocosta.com.br>
2018-01-22 00:16:43 -0300

    Adicionado seeds

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO `usuario` VALUES 
+    (1,'Joe','joe@doe.com','A','2016-03-25 13:23:14','$2y$10$teABp70WoSyA/9UpH0SaiuQ2aBdJn3tOb2a4opAcy/VO9bjtUhGGe');
+
+INSERT INTO `vacina` VALUES 
+    (1,'Vanguard','2016-03-25 15:03:35'),
+    (2,'Anti-rábica','2016-03-25 15:03:44'),
+    (3,'Leshimune','2016-03-25 15:04:15');
