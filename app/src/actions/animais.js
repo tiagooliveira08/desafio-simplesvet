feef04a80d3fbf6d247fe5e63998bf9b125fa673
Diego Costa <diego@diegocosta.com.br>
2018-01-21 23:29:22 -0300

    Adicionado action/reducer para pegar um único animal e fazer upload de sua foto

--- app/src/actions/animais.js
+++ app/src/actions/animais.js
@@ -1,4 +1,5 @@
-import { getList } from '../services';
+import { toastr } from 'react-redux-toastr';
+import { getList, getEntry, upload } from '../services';
 
 import { RacasAction, ProprietariosAction } from './index';
 
@@ -7,10 +8,41 @@ export const fetchList = () => ({
     payload: getList('animais')
 });
 
+export const fetchEntry = (id) => ({
+    type: 'ANIMAL_FETCHED',
+    payload: getEntry('animais', id)
+});
+
 export const fetchAnimaisData = () => {
     return [
         RacasAction.fetchList(),
         ProprietariosAction.fetchList(),
         fetchList(),
     ];
+};
+
+export const fetchAnimailData = (id) => {
+    return [
+        RacasAction.fetchList(),
+        ProprietariosAction.fetchList(),
+        fetchEntry(id)
+    ];
+};
+
+export const uploadAnimalImage = (file) => {
+    return dispatch => {
+        upload(file)
+            .then(resp => {
+                toastr.success('Sucesso', 'Imagem enviada para nossos servidores!');
+                console.log(resp);
+
+                dispatch({
+                    type: 'ANIMAL_IMAGE_UPLOADED',
+                    payload: resp.data.file
+                });
+            })
+            .catch(e => {
+                toastr.error('Erro!', e.response.data.error);
+            })
+    }
 }
--- app/src/reducers/animais.js
+++ app/src/reducers/animais.js
@@ -1,12 +1,19 @@
 const INITIAL_STATE = { 
-    list: []
-}
+    list: [],
+    current: {},
+};
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case 'ANIMAIS_FETCHED' : 
             return { ...state, list: action.payload.data }
+        case 'ANIMAL_FETCHED':
+            return {...state, current: action.payload.data };
+        case 'ANIMAL_IMAGE_UPLOADED':
+            const current = state.current;
+            current.foto = action.payload;
+            return {...state, current };
         default:
             return state
     }
-}
+};
