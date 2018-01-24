e4eb216308dd82e66186cb082e1febdaf91c4c74
Diego Costa <diego@diegocosta.com.br>
2018-01-23 23:24:18 -0300

    Removido o Axios como dependencia de dev

--- app/src/services/index.js
+++ app/src/services/index.js
@@ -1,27 +1,8 @@
-
-import Axios from 'axios';
+import { create } from 'apisauce';
 
 export const API_URL = "http://simplesvet.local/api";
 export const UPLOADS_URL = API_URL + '/public/uploads/';
 
-export const login = userData => Axios.post(`${API_URL}/auth/login`, userData)
-export const validate = token => Axios.post(`${API_URL}/auth/validate`, { token })
-export const getList = (endpoint) => Axios.get(`${API_URL}/${endpoint}`);
-export const getEntry = (endpoint, id) => Axios.get(`${API_URL}/${endpoint}/${id}`);
-export const createEntry = (endpoint, data) => Axios.post(`${API_URL}/${endpoint}`, data);
-export const updateEntry = (endpoint, data, id) => Axios.put(`${API_URL}/${endpoint}/${id}`, data);
-export const deleteEntry = (endpoint, id) => Axios.delete(`${API_URL}/${endpoint}/${id}`);
-
-export const upload = (file) => {
-    const formData = new FormData();
-    formData.append('foto',file)
-
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
-    }
-
-    return Axios.post(`${API_URL}/upload`, formData, config);
-};
-
+export const service = create({
+    baseURL: API_URL
+});
