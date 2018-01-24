40758e97eadf25d1906a091ec356a9bd654de3f3
Diego Costa <diego@diegocosta.com.br>
2018-01-23 22:59:38 -0300

    Refatorado o action de auth

--- app/src/actions/auth.js
+++ app/src/actions/auth.js
@@ -1,44 +1,25 @@
 import { toastr } from 'react-redux-toastr';
+import { service } from '../services';
 
-import { login, validate } from '../services';
-
-export const loginAction = (values) => {
+export const login = values => {
     return dispatch => {
-        login(values).then(resp => {
-            dispatch([
-                {
-                    type: 'USER_FETCHED',
-                    payload: resp.data
-                }
-            ])
-        })
-        .catch(e => {
-            toastr.error('Error', e)
-        })
-    }
-}
+        service.post('/auth/login', values)
+            .then(resp => dispatch({ type: 'USER_FETCHED', payload: resp.data }))
+            .catch(e => toastr.error('Error', e));
+    };
+};
 
-export const validateTokenAction = token => {
+export const validate = token => {
     return dispatch => {
         if(token) {
-            validate(token).then(resp => {
-                dispatch({
-                    type: 'TOKEN_VALIDATED',
-                    payload: resp.data.status
-                })
-            })
-            .catch(e => dispatch({
-                type: 'TOKEN_VALIDATED',
-                payload: false
-            }))
+            service.post('/auth/validate', { token })
+                .then(resp => dispatch({ type: 'TOKEN_VALIDATED', payload: resp.data.status }))
+                .catch(e => dispatch({ type: 'TOKEN_VALIDATED', payload: false }))
         }
         else {
-            dispatch({
-                type: 'TOKEN_VALIDATED',
-                payload: false
-            })
+            dispatch({ type: 'TOKEN_VALIDATED', payload: false })
         }
     }
 }
 
-export const logoutAction = () => ({ type: 'TOKEN_VALIDATED', payload: false })
+export const logout = () => ({ type: 'TOKEN_VALIDATED', payload: false })
