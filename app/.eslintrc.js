0b65c8e2c0db812b1551f764f36c220713e8c30f
Diego Costa <diego@diegocosta.com.br>
2018-01-25 08:54:13 -0300

    Adicionado configuração pro ESlint permitir JSX em arquivos .js

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     "extends": "airbnb",
     "env": {
       "browser": true,
-    }
+    },
+    "rules": {
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "no-underscore-dangle": ["error", { "allow": ["__REDUX_DEVTOOLS_EXTENSION__"] }],
+  },
 };
