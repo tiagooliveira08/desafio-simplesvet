390d08755fa824066b38b3c8af5c4aa59d801c42
Diego Costa <diego@diegocosta.com.br>
2018-01-27 07:13:10 -0300

    Adicionado mensagem na home

--- app/src/containers/Home/index.js
+++ app/src/containers/Home/index.js
@@ -9,6 +9,9 @@ export default () => (
     <PageHeader title="Bem-Vindo" />
     <Page>
       <p>SimplesVet App em React :)</p>
+      <p>Essa é uma aplicação bastante simples e com possíveis
+          falhas pois foi feita com muita pressa para prova de conceito.
+      </p>
     </Page>
   </div>
 );
